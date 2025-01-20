275c48a1bbeb0bcc749d1de0f8f3af1b7e8b995b
ysk9926 <74546856+ysk9926@users.noreply.github.com>
2025-01-20 15:58:12 +0900

    Initial init
    - 스프링부트 파일 생성
    - 기본적인 그래들 설정 추가
    - yml 유지보수 및 보안을 위한 서브모듈 추가

---
+++ src/main/java/org/cycle_route/back_end/BackEndApplication.java
@@ -0,0 +1,13 @@
+package org.cycle_route.back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackEndApplication.class, args);
+    }
+
+}
